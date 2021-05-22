d6ade5e131417ed849bc0622ad1f26c8a1cb3fc4
miltonsmendes <miltonsergiom@gmail.com>
2021-05-22 15:25:10 -0300

    Initial commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,41 @@
+import { Container } from "./styles";
+import { useEffect } from 'react';
+import { api } from "../../services/api";
+
+export function TransactionTable() {
+
+    useEffect(() => {
+        api.get('transactions')
+            .then(response => console.log(response.data))
+    }, [])
+
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td className="deposit">Desenvolvimento de Website</td>
+                        <td>R$ 12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/05/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$ 1.100</td>
+                        <td>Casa</td>
+                        <td>17/05/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
